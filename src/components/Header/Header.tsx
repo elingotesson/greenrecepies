6150198783968002a5800c8554d8ddf5c966cea6
Elin Götesson <elin.gotesson@fortnox.se>
2021-11-29 19:09:57 +0100

    Add missing files

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./Header.module.scss";
+
+export const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Green Recepies</h1>
+      <ul className="nav">
+        <li>
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/recepies">Recepies</NavLink>
+        </li>
+      </ul>
+    </header>
+  );
+};
