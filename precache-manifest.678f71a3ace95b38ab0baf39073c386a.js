5636e7e6291b3fb791dfd9454334208c00680c04
Elin Götesson <elin.gotesson@fortnox.se>
2021-11-30 12:20:22 +0100

    Updates

---
+++ precache-manifest.678f71a3ace95b38ab0baf39073c386a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219a853dfdfdf4e19c1c686fe20fddc3",
+    "url": "/greenrecepies/index.html"
+  },
+  {
+    "revision": "75e88ed15e9d0eb3254e",
+    "url": "/greenrecepies/static/css/2.94ae5829.chunk.css"
+  },
+  {
+    "revision": "0928274976ba16a12342",
+    "url": "/greenrecepies/static/css/main.1f720388.chunk.css"
+  },
+  {
+    "revision": "75e88ed15e9d0eb3254e",
+    "url": "/greenrecepies/static/js/2.bfe37a87.chunk.js"
+  },
+  {
+    "revision": "0928274976ba16a12342",
+    "url": "/greenrecepies/static/js/main.8a2ad4ae.chunk.js"
+  },
+  {
+    "revision": "36122fdc56b3802fef83",
+    "url": "/greenrecepies/static/js/runtime~main.0374ff8e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/greenrecepies/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/greenrecepies/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/greenrecepies/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/greenrecepies/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/greenrecepies/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
